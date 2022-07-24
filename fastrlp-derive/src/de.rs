468fdf24608737d979e9b41276c3b91a47981c55
Artem Vorotnikov <artem@vorotnikov.me>
2022-07-24 09:11:46 +0300

    Initial commit

---
+++ fastrlp-derive/src/de.rs
@@ -0,0 +1,100 @@
+use alloc::vec::Vec;
+use proc_macro2::TokenStream;
+use quote::quote;
+
+pub fn impl_decodable(ast: &syn::DeriveInput) -> TokenStream {
+    let body = if let syn::Data::Struct(s) = &ast.data {
+        s
+    } else {
+        panic!("#[derive(RlpDecodable)] is only defined for structs.");
+    };
+
+    let stmts: Vec<_> = body
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(i, field)| decodable_field(i, field))
+        .collect();
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let impl_block = quote! {
+        impl #impl_generics fastrlp::Decodable for #name #ty_generics #where_clause {
+            fn decode(mut buf: &mut &[u8]) -> Result<Self, fastrlp::DecodeError> {
+                let b = &mut &**buf;
+                let rlp_head = fastrlp::Header::decode(b)?;
+
+                if !rlp_head.list {
+                    return ::core::result::Result::Err(fastrlp::DecodeError::UnexpectedString);
+                }
+
+                let started_len = b.len();
+                let this = Self {
+                    #(#stmts)*
+                };
+
+                let consumed = started_len - b.len();
+                if consumed != rlp_head.payload_length {
+                    return ::core::result::Result::Err(fastrlp::DecodeError::ListLengthMismatch {
+                        expected: rlp_head.payload_length,
+                        got: consumed,
+                    });
+                }
+
+                *buf = *b;
+
+                ::core::result::Result::Ok(this)
+            }
+        }
+    };
+
+    quote! {
+        const _: () = {
+            extern crate fastrlp;
+            #impl_block
+        };
+    }
+}
+
+pub fn impl_decodable_wrapper(ast: &syn::DeriveInput) -> TokenStream {
+    let body = if let syn::Data::Struct(s) = &ast.data {
+        s
+    } else {
+        panic!("#[derive(RlpEncodableWrapper)] is only defined for structs.");
+    };
+
+    assert_eq!(
+        body.fields.iter().count(),
+        1,
+        "#[derive(RlpEncodableWrapper)] is only defined for structs with one field."
+    );
+
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let impl_block = quote! {
+        impl #impl_generics fastrlp::Decodable for #name #ty_generics #where_clause {
+            fn decode(buf: &mut &[u8]) -> Result<Self, fastrlp::DecodeError> {
+                ::core::result::Result::Ok(Self(fastrlp::Decodable::decode(buf)?))
+            }
+        }
+    };
+
+    quote! {
+        const _: () = {
+            extern crate fastrlp;
+            #impl_block
+        };
+    }
+}
+
+fn decodable_field(index: usize, field: &syn::Field) -> TokenStream {
+    let id = if let Some(ident) = &field.ident {
+        quote! { #ident }
+    } else {
+        let index = syn::Index::from(index);
+        quote! { #index }
+    };
+
+    quote! { #id: fastrlp::Decodable::decode(b)?, }
+}
---
+++ fastrlp-derive/src/en.rs
@@ -0,0 +1,161 @@
+use alloc::vec::Vec;
+use proc_macro2::TokenStream;
+use quote::quote;
+
+pub fn impl_encodable(ast: &syn::DeriveInput) -> TokenStream {
+    let body = if let syn::Data::Struct(s) = &ast.data {
+        s
+    } else {
+        panic!("#[derive(RlpEncodable)] is only defined for structs.");
+    };
+
+    let length_stmts: Vec<_> = body
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(i, field)| encodable_length(i, field))
+        .collect();
+
+    let stmts: Vec<_> = body
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(i, field)| encodable_field(i, field))
+        .collect();
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let impl_block = quote! {
+        trait E {
+            fn rlp_header(&self) -> fastrlp::Header;
+        }
+
+        impl #impl_generics E for #name #ty_generics #where_clause {
+            fn rlp_header(&self) -> fastrlp::Header {
+                let mut rlp_head = fastrlp::Header { list: true, payload_length: 0 };
+                #(#length_stmts)*
+                rlp_head
+            }
+        }
+
+        impl #impl_generics fastrlp::Encodable for #name #ty_generics #where_clause {
+            fn length(&self) -> usize {
+                let rlp_head = E::rlp_header(self);
+                return fastrlp::length_of_length(rlp_head.payload_length) + rlp_head.payload_length;
+            }
+            fn encode(&self, out: &mut dyn fastrlp::BufMut) {
+                E::rlp_header(self).encode(out);
+                #(#stmts)*
+            }
+        }
+    };
+
+    quote! {
+        const _: () = {
+            extern crate fastrlp;
+            #impl_block
+        };
+    }
+}
+
+pub fn impl_encodable_wrapper(ast: &syn::DeriveInput) -> TokenStream {
+    let body = if let syn::Data::Struct(s) = &ast.data {
+        s
+    } else {
+        panic!("#[derive(RlpEncodableWrapper)] is only defined for structs.");
+    };
+
+    let ident = {
+        let fields: Vec<_> = body.fields.iter().collect();
+        if fields.len() == 1 {
+            let field = fields.first().expect("fields.len() == 1; qed");
+            field_ident(0, field)
+        } else {
+            panic!("#[derive(RlpEncodableWrapper)] is only defined for structs with one field.")
+        }
+    };
+
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let impl_block = quote! {
+        impl #impl_generics fastrlp::Encodable for #name #ty_generics #where_clause {
+            fn length(&self) -> usize {
+                self.#ident.length()
+            }
+            fn encode(&self, out: &mut dyn fastrlp::BufMut) {
+                self.#ident.encode(out)
+            }
+        }
+    };
+
+    quote! {
+        const _: () = {
+            extern crate fastrlp;
+            #impl_block
+        };
+    }
+}
+
+pub fn impl_max_encoded_len(ast: &syn::DeriveInput) -> TokenStream {
+    let body = if let syn::Data::Struct(s) = &ast.data {
+        s
+    } else {
+        panic!("#[derive(RlpEncodable)] is only defined for structs.");
+    };
+
+    let stmts: Vec<_> = body
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(index, field)| encodable_max_length(index, field))
+        .collect();
+    let name = &ast.ident;
+
+    let impl_block = quote! {
+        unsafe impl fastrlp::MaxEncodedLen<{ fastrlp::const_add(fastrlp::length_of_length(#(#stmts)*), #(#stmts)*) }> for #name {}
+        unsafe impl fastrlp::MaxEncodedLenAssoc for #name {
+            const LEN: usize = { fastrlp::const_add(fastrlp::length_of_length(#(#stmts)*), { #(#stmts)* }) };
+        }
+    };
+
+    quote! {
+        const _: () = {
+            extern crate fastrlp;
+            #impl_block
+        };
+    }
+}
+
+fn field_ident(index: usize, field: &syn::Field) -> TokenStream {
+    if let Some(ident) = &field.ident {
+        quote! { #ident }
+    } else {
+        let index = syn::Index::from(index);
+        quote! { #index }
+    }
+}
+
+fn encodable_length(index: usize, field: &syn::Field) -> TokenStream {
+    let ident = field_ident(index, field);
+
+    quote! { rlp_head.payload_length += fastrlp::Encodable::length(&self.#ident); }
+}
+
+fn encodable_max_length(index: usize, field: &syn::Field) -> TokenStream {
+    let fieldtype = &field.ty;
+
+    if index == 0 {
+        quote! { <#fieldtype as fastrlp::MaxEncodedLenAssoc>::LEN }
+    } else {
+        quote! { + <#fieldtype as fastrlp::MaxEncodedLenAssoc>::LEN }
+    }
+}
+
+fn encodable_field(index: usize, field: &syn::Field) -> TokenStream {
+    let ident = field_ident(index, field);
+
+    let id = quote! { self.#ident };
+
+    quote! { fastrlp::Encodable::encode(&#id, out); }
+}
